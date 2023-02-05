6d4afb2961fdc737f4296d9bfb2b052852fff174
João Vinícius Guerber de Souza <joaoviniciusgs@gmail.com>
2023-02-05 14:27:52 -0300

    finalizado

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -13,18 +13,30 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    return await this.repository
+      .createQueryBuilder("games")
+      .select("games.title")
+      .where(`games.title ilike :title`, { title: `%${param}%` })
+      .getMany()
+    // Complete usando query builder
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query(`
+      SELECT
+        COUNT (*)
+      FROM 
+        games;
+    `); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    const queryBuilder = await getRepository(User)
+      .createQueryBuilder('users')
+      .leftJoinAndSelect('users.games', 'games')
+      .where('games.id = :id', { id })
+      .getMany()
+
+    return queryBuilder
   }
 }
